232da8aaacd53db76ad4b4ee9e32de93a91dc874
Charles Lee <charleslee94@berkeley.edu>
2015-10-18 21:50:06 -0700

    wrote get_player_ids which gets player_ids; get_player_page returns matches of all players in a single file

--- rating_calculator.py
+++ rating_calculator.py
@@ -1,4 +1,5 @@
 import json
+from IPython.core.debugger import Tracer
 from trueskill import rate_1vs1, Rating
 from requests import get
 from config import CHALLONGE_KEY
@@ -15,6 +16,71 @@ def get_tournament_data(tournament_id):
     players = get(url.format(tournament_id, 'participants'), params=params)
 
     return matches.json(), players.json()
+
+#def consolidate_user_ids(*jsonFiles):
+#    """ Consolidates user_ids from many tournaments
+#
+#    """
+#    consolidated_ids ={}
+#    initial = 0
+#    for jsonFile in jsonFiles:
+#        if initial == 0:
+#            consolidated_ids = get_player_ids(jsonFile)
+#            continue
+#        nextIdList = get_player_ids(jsonFile)
+#            
+
+
+def get_player_ids(jsonFile):
+    """ Gets user_ids from Json File.
+
+    :param Json File String: name of the json file as a string
+    :returns dictionary of {player_id from tournament : user_id}
+    """
+    with open(jsonFile) as data:
+        players = json.load(data)
+    player_ids = {}
+    for player in players:
+        player_ids[player['participant']['id']] = player['participant']['user_id'], player['participant']['id']
+    return player_ids
+
+def get_player_page(player_ids):
+    """ Using player ids, creates player page.
+
+    :param Json with player_ids added
+    :returns dictionary playerPages with {player_id: matches}
+    Tournament name is currently hard-coded, can't figure out how to relate tournament id int into string form
+
+    """
+    
+    playerPages = {}
+    for player in player_ids:
+        playerPages[player_ids[player][0]] = []
+        curMatch = []
+        for match in matches:
+            if player == match['match']['player1_id'] or player ==match['match']['player2_id']:
+                if player == match['match']['player1_id']:
+                    opponent = match['match']['player2_id']
+                else:
+                    opponent = match['match']['player1_id']
+                opponent = player_ids[opponent][0]
+                winner = match['match']['winner_id']
+                if winner == player:
+                    outcome = 'win'
+                else:
+                    outcome = 'lose'
+                tournament = match['match']['tournament_id']
+                if str(tournament) == '1976976':
+                    tournament = 'hacksmoney'
+                
+                curMatch= {'opp' : opponent, 'outcome' : outcome, 'tournament' : tournament}
+                matches = {}
+                playerPages[player_ids[player][0]].append(curMatch)
+    #write to json
+    with open('player_ids_test.json', 'w') as outfile:
+        json.dump(playerPages, outfile)
+    return playerPages
+    
 
 
 def build_player_map(players):
@@ -82,3 +148,6 @@ if __name__ == "__main__":
     pmap = build_player_map(players)
     pmap = process_match_data(matches, pmap)
     pmap = transform_ts_to_dict(pmap)
+    player_ids = get_player_ids('hacksmoneyUser_ids.json')
+    playerPage = get_player_page(player_ids)
+
